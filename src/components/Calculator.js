e0b1fb93dae9032a5528283b5393529047f90251
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-07-12 21:57:32 +0530

    feat: create new container component

---
+++ src/components/Calculator.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Calculator.css";
+
+export default function Calculator() {
+  return (
+    <>
+      <div className="container">
+        
+      </div>
+    </>
+  )
+}
+
