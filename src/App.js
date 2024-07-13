7d161f116459d97d6dce5e63a4182a62261f8d2d
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-07-13 15:20:14 +0530

    feat: add state from handle components from on submit click

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Calculator from "./components/Calculator";
+import Result from "./components/Result";
 import "./App.css";
 
 function App() {
+  const [currentComponent, setCurrentComponent] = useState("calculator");
+
+  const handleSubmitClick = () => {
+    setCurrentComponent("resut");
+  };
+
+  let renderComponent;
+  if (currentComponent === "calculator") {
+    renderComponent = <Calculator onSubmitClick={handleSubmitClick} />;
+  } else if (currentComponent === "result") {
+    renderComponent = <Result />;
+  }
+
   return (
     <div className="App">
-      <div className="container">
-        <Calculator />
-      </div>
+      <div className="container">{renderComponent}</div>
     </div>
   );
 }
