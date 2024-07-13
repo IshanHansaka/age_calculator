f811768157f1a2228a1811b9b906a8e565cec2ed
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-07-13 18:58:50 +0530

    chore: change test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders age calculator title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/age calculator/i);
+  expect(titleElement).toBeInTheDocument();
 });
