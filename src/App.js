8c61ba0c511a423cb7563e157444e23a3329b3ea
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-07-13 14:43:22 +0530

    feat: add container

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Calculator />} />
-        </Routes>
-      </Router>
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Calculator />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 }
